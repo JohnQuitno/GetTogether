acba20680fce8c6599d3e3b56e3ad482c7585c89
giangpro93 <giangnt93@ku.edu>
2019-02-10 13:10:44 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 (function() {
 	// Initialize Firebase
 	var config = {
-		apiKey: "AIzaSyCvzWllTU02Y_FMjKkgMFzQHmUYgxPXX1U",
-		authDomain: "test-e9e28.firebaseapp.com",
-		databaseURL: "https://test-e9e28.firebaseio.com",
-		projectId: "test-e9e28",
-		storageBucket: "test-e9e28.appspot.com",
-		messagingSenderId: "309539995990"
+		apiKey: "",
+		authDomain: "",
+		databaseURL: "",
+		projectId: "",
+		storageBucket: "",
+		messagingSenderId: ""
 	 };
 	 firebase.initializeApp(config);
 }());
